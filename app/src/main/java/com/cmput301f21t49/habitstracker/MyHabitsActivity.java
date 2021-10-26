9e0443f3bbffe7171f4aef9a68125d23cf5478aa
Purvi Singh <purvi@ualberta.ca>
2021-10-26 12:24:22 -0600

    Main activity and menu

---
+++ app/src/main/java/com/cmput301f21t49/habitstracker/MyHabitsActivity.java
@@ -0,0 +1,14 @@
+package com.cmput301f21t49.habitstracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyHabitsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myhabits);
+
+    }
+}
