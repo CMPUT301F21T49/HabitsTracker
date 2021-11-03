b24ec629a5dc19020439b12cb84bc3f9dfde54cc
fzhu886 <71693974+fzhu886@users.noreply.github.com>
2021-11-03 05:00:23 -0600

    Update EditViewHabitEventActivity.java

--- app/src/main/java/com/cmput301f21t49/habitstracker/EditViewHabitEventActivity.java
+++ app/src/main/java/com/cmput301f21t49/habitstracker/EditViewHabitEventActivity.java
@@ -1,13 +1,34 @@
 package com.cmput301f21t49.habitstracker;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EditViewHabitEventActivity extends AppCompatActivity {
+
+    Button Save;
+    Button Escape;
+    public User currentUser;
+    List<String> habitNameList = new ArrayList<>();
+    ArrayList<Habit> habitArrayList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_view_event);
+
+        currentUser = (User) getIntent().getSerializableExtra("CurrentUserObj");
+        if (currentUser != null && currentUser.getHabits()!= null){
+            for (Habit h : habitArrayList = currentUser.getHabits()) {
+                habitNameList.add(h.getName());
+            }
+
+        }
+        
+
     }
 }
