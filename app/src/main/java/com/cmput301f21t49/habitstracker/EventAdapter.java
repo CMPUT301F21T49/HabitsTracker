aafb2034c21bb22f9c77e7164332ea2772b18f0a
fzhu886 <71693974+fzhu886@users.noreply.github.com>
2021-11-03 16:17:10 -0600

    Update EventAdapter.java

--- app/src/main/java/com/cmput301f21t49/habitstracker/EventAdapter.java
+++ app/src/main/java/com/cmput301f21t49/habitstracker/EventAdapter.java
@@ -1,8 +1,32 @@
 package com.cmput301f21t49.habitstracker;
 
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-//public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
+    @Override
+    @NonNull
+    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
 
-//}
+    @Override
+    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    
+    public class EventViewHolder extends RecyclerView.ViewHolder{
+        public EventViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
 
