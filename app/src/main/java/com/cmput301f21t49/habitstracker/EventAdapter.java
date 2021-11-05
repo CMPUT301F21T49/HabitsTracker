52deb0b5daf258752d02f27b9e570befdcce8ffb
fzhu886 <71693974+fzhu886@users.noreply.github.com>
2021-11-05 15:33:15 -0600

    Update EventAdapter.java

--- app/src/main/java/com/cmput301f21t49/habitstracker/EventAdapter.java
+++ app/src/main/java/com/cmput301f21t49/habitstracker/EventAdapter.java
@@ -1,32 +1,39 @@
-package com.cmput301f21t49.habitstracker;
-
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+
+public class EventAdapter extends ArrayList<Event> {
+
+    ArrayList<Event> tempList;
+
+
+    public EventAdapter(ArrayList<Event> tempList, Context context) {
+        super(context, 0, tempList);
+        this.tempList = tempList;
+    }
+
     @Override
     @NonNull
-    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-    
-    public class EventViewHolder extends RecyclerView.ViewHolder{
-        public EventViewHolder(@NonNull View itemView) {
-            super(itemView);
+    public View getView(int position, @NonNull ViewGroup parent, @Nullable View convertView) {
+        View v = convertView;
+        if (v == null) {
+            v = LayoutInflater.from(parent.getContext()).inflate(R.layout.todays_events, parent, false);
         }
+
+        return v;
     }
+
+
 }
+
+
 
